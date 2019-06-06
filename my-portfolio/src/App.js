effa1001c12448d6a3501338cabf09831120254a
Kenneth Solano <klsolano77@gmail.com>
2019-06-05 22:38:53 -0400

    s

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -10,8 +10,25 @@ function App() {
           backgroundSize: "cover",
           height: "100vh",
           width: "100%",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed"
 
         }}>
+          <div className="box1">
+            <p style={{textAlign:"center", width: "25vw"}}>Hey there! Thank you for visiting my page today! 
+              This is my portfolio that contains my 3 projects I created at Ironhack Miami. 
+              Also below you will see see links to my Github, LinkedIn, and resume.
+              </p>
+          </div>
+          <div className="box2">
+          <h1>Kenneth Solano</h1>
+          </div>
+          <div className="box3">
+          <p style={{textAlign:"center", }}>At Ironhack I have been able to learn many languages which include:
+          javaScript, React.js, HTML, CSS, AJAX, Node.js, Handlebars, Express.js, Mongodb. 
+          As I continue this journey I will keep on inproving these languages and leanr much more.
+              </p>
+          </div>
 
       </div>
       <div className="section2"style={{
@@ -19,6 +36,8 @@ function App() {
           backgroundSize: "cover",
           height: "100vh",
           width: "100%",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed"
 
         }}>
 
@@ -28,6 +47,8 @@ function App() {
           backgroundSize: "cover",
           height: "100vh",
           width: "100%",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed"
 
         }}>
 
@@ -37,8 +58,18 @@ function App() {
           backgroundSize: "cover",
           height: "100vh",
           width: "100%",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed"
 
         }}>
+
+      </div>
+      <div className="bottomNav">
+        <li>
+          <ul>x</ul>
+          <ul>x</ul>
+          <ul>x</ul>
+        </li>
 
       </div>
       
