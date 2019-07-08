b79bbb75d5263ed22cdee46916f8db06917d4263
Kenneth Solano <klsolano77@gmail.com>
2019-07-08 16:40:50 -0400

    s

--- my-portfolio/src/Components/Technologies.jsx
+++ my-portfolio/src/Components/Technologies.jsx
@@ -39,7 +39,7 @@ export default class Technologies extends Component {
             </div>
           </div>
           
-          <div class="container-fluid">
+          {/* <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-4 mt-5" >
 			<div class="card">
@@ -87,7 +87,7 @@ export default class Technologies extends Component {
 			</div>
 		</div>
 	</div>
-</div>
+</div> */}
 
           {/* <Container>
 
